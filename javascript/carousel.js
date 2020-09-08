2208fb5aedd2580070d40cdffff4b2d4095c1df0
Brad <bjkapteyn@gmail.com>
2020-09-08 19:37:39 -0400

    added rulesets for carousel elements

---
+++ javascript/carousel.js
@@ -0,0 +1,26 @@
+let slideButtons = {
+    left: document.getElementById('leftButton'),
+    right: document.getElementById('rightButton')
+}
+
+let carouselElements = {
+
+}
+
+slideButtons.left.addEventListener('click', function(event) {
+    event.preventDefault();
+    slideLeft();
+})
+
+slideButtons.right.addEventListener('click', function(event) {
+    event.preventDefault();
+    slideRight()
+})
+
+function slideRight() {
+
+}
+
+function slideLeft() {
+
+}
