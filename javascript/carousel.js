defd88a68be02be18af02480d594efbc78d4d68a
Brad <bjkapteyn@gmail.com>
2020-09-08 22:07:28 -0400

    refactoring utilitites into constructor

--- javascript/carousel.js
+++ javascript/carousel.js
@@ -2,8 +2,9 @@ let slideButtons = {
     left: document.getElementById('leftButton'),
     right: document.getElementById('rightButton')
 }
-
-let carouselElements = {
+debugger;
+//contains elements and views with incrementing function
+let carouselUtilities = new {
     carousel: document.getElementById('carousel'),
     carouselShowing: document.getElementById('carouselShowing'),
     carouselView1: document.getElementById('carouselShow1'),
@@ -11,11 +12,12 @@ let carouselElements = {
     carouselView3: document.getElementById('carouselShow3'),
     carouselView4: document.getElementById('carouselShow4'),
     carouselView5: document.getElementById('carouselShow5'),
-    carouselViews: [carouselView1, carouselView2, carouselView3, carouselView4, carouselView5],
+    //
+    carouselViews: [this.carouselView1, this.carouselView2, this.carouselView3, this.carouselView4, this.carouselView5],
     currentViewIndex: 0,
-    previousViewIndex: carouselViews.length - 1,
-    nextViewIndex = 1,
-    changeViewIndex: function(upOrDown = true, index = 0) {
+    leftViewIndex: this.carouselViews.length - 1,
+    rightViewIndex: 1,
+    changeIndex: function(upOrDown = true, index = 0) {
         if(upOrDown) {
             if(index == this.carouselViews.length - 1) {
                 return 0;
@@ -32,24 +34,39 @@ let carouselElements = {
                 return index - 1;
             }
         }
+    },
+    changeViewIndexes: function(upOrDown = true) {
+        this.currentViewIndex = this.changeIndex(upOrDown, this.currentViewIndex);
+        this.leftViewIndex = this.changeIndex(upOrDown, this.leftViewIndex);
+        this.rightViewIndex = this.changeIndex(upOrDown, this.rightViewIndex);
     }
 }
 
 slideButtons.left.addEventListener('click', function(event) {
     event.preventDefault();
-    slideLeft();
+    slideCarousel(true);
 })
 
 slideButtons.right.addEventListener('click', function(event) {
     event.preventDefault();
-    slideRight()
+    slideCarousel(false)
 })
 
-function slideRight() {
+//true is slide right false is slide left
+function slideCarousel(leftOrRight) {
+    let currentView = carouselUtilities.carouselViews[carouselUtilities.currentViewIndex];
+    if(leftOrRight) {
+        let leftView = carouselUtilities.carouselViews[carouselUtilities.rightViewIndex];
+        currentView.classList.add("right");
+        currentView.classList.remove("center");
+    
+    }
+    document.getElementById().classList.re
 
-
+    //increment indexes
+    carouselUtilities.changeViewIndexes(leftOrRight);
 }
 
 function slideLeft() {
-
+    carouselUtilities.changeViewIndexes(false);
 }
