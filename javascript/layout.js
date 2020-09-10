0c8593077c1b8f205dfb9aa51ceb9e346f87702b
Brad <bjkapteyn@gmail.com>
2020-09-10 17:50:21 -0400

    finished navbar dock

--- javascript/layout.js
+++ javascript/layout.js
@@ -1,34 +1,38 @@
 let navElements = {
     navBarContainer: document.getElementById('navContainer'),
     navBar: document.getElementById('navBar'),
-    navButtons: document.querySelectorAll("#navButton")
+    navButtons: document.querySelectorAll(".navButton")
 }
 
 function dockElement() {
     navElements.navBar.style.backgroundColor = "rgba(0,0,0,0.0)";
     navElements.navBarContainer.style.right = "155px";
 
-    for(let i = 0; i < navElements.navButtons.length - 1; i++) {
-        navElements.navButtons.item(i).style.color = "rgb(51, 65, 149)";
+    for(let i = 0; i < navElements.navButtons.length; i++) {
+        navElements.navButtons.item(i).style.color = "";
     }
 }
 
 function unDockElement() {
     navElements.navBarContainer.style.right = "50%";
     navElements.navBar.style.backgroundColor = "rgba(0,0,0,0.5)";
+    debugger;
 
-    for(let i = 0; i < navElements.navButtons.length - 1; i++) {
+    for(let i = 0; i < navElements.navButtons.length; i++) {
         navElements.navButtons.item(i).style.color = "white";
     }
 }
 
 function dockUnDock() {
     console.log(window.pageYOffset);
-    if (window.pageYOffset >= 175) {
-        unDockElement();
-    }
-    else {
-        dockElement();
+    let offset = window.pageYOffset;
+    if(offset >= 100 || offset <= 300) {
+        if (window.pageYOffset >= 175) {
+            unDockElement();
+        }
+        else {
+            dockElement();
+        }
     }
 }
 
