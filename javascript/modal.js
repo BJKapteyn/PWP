12fa11cece0263da4d0480804bf15eeeded4d898
Brad <bjkapteyn@gmail.com>
2020-09-20 15:30:36 -0400

    added js file for modal on contact page

---
+++ javascript/modal.js
@@ -0,0 +1,17 @@
+let vectorZones = {
+    zoneB: document.querySelector(""),
+    zoneC: document.querySelector(""),
+    zoneC: document.querySelector(""),
+    zoneC: document.querySelector(""),
+    zoneC: document.querySelector("")
+}
+
+window.onload = function {
+    vectorZones = {
+        zoneB: document.querySelector("#zoneB"),
+        zoneC: document.querySelector("#zoneC"),
+        zoneC: document.querySelector("#zoneD"),
+        zoneC: document.querySelector("#zoneF"),
+        zoneC: document.querySelector("#zoneH")
+    }
+}
