f1c46bad415102a97a66a5175146ac2c540b7f6e
Brad <bjkapteyn@gmail.com>
2020-09-20 17:44:07 -0400

    modalElements work

--- javascript/modal.js
+++ javascript/modal.js
@@ -17,30 +17,41 @@ let vectorZones = {
 }
 
 //contains info on what should go in the modal
-function contactModal(titleStr, zoneStatesStr, zonePresStr, phoneNumberStr) {
-    this.title = titleStr;
+function contactModal(zoneStr, zoneStatesStr, zonePresStr, phoneNumberStr = "555-555-5555", zoneChaptersStr) {
+    this.zone = zoneStr;
     this.zoneStates = zoneStatesStr;
+    this.zoneChapters = zoneChaptersStr;
     this.zonePres = zonePresStr;
-    this.contactPhone = `tel:+${phoneNumberStr}`;
+    this.contactPhone = phoneNumberstr;
+    this.contactSrc = `tel:+${phoneNumberStr}`;
 }
 
 function modalElements(contactModal = new contactModal()) {
-    let title = document.createElement("h2");
+    let zone = document.createElement("h2");
     let zoneStatesTitle = document.createElement("h3");
     let zoneStates = document.createElement("p");
+    let zonePresTitle = document.createElement("h3");
     let phoneTitle = document.createElement("h3");
-    let phoneNumber = document.createElement("p");
+    let phoneNumber = document.createElement("a");
     let emailTitle = document.createElement("h3");
     let email = document.createElement("p");
-    let elements = [this.title, this.zoneStatesTitle, this.zoneStates, this.phoneTitle, this.phoneNumber, this.emailTitle, this.email];
     let modal = document.createElement("div");
+    modal.id = "contactModal";
     
+    zone.innerText = contactModal.zone;
+    zoneStatesTitle.innerText = "States Covered:";
+    zoneStates.innerText = contactModal.zoneStates;
+    zonePres
+
+    phoneTitle.innerText = "Contact Number";
+    phoneNumber.src = contactModal.contactPhone;
+    phoneNumber
     // title.className = "modalTitle";
     // zoneStatesTitle.className = "modalSubtitle";
     // phoneTitle.className = "modalSubtitle";
-
-    modal.id = "contactModal";
-
+    
+    
+    let elements = [zone, zoneStatesTitle, zoneStates, zonePresTitle, zonePres, phoneTitle, phoneNumber,  emailTitle, email];
     for(let i = 0; i < elements.length; i++) {
         modal.appendChild(elements[i]);
     }
@@ -54,4 +65,19 @@ function createModal(contactModal = new contactModal()) {
 
 window.onload = function() {
 
+}
+
+function phoneNumberBuilder(pNumberStr = "555-555-5555") {//build phone number, returns default phone number if nothing is given.
+    pNumberArr = pNumberStr.split('-');
+    let result;
+    try {
+        if(pNumberStr == "555-555-5555") {
+            throw "default number";
+        }
+        result = `(${pNumberArr[0]}) ${pNumberArr[1]}-${pNumberArr[3]}`
+    }
+    catch {
+        result = "(800) 637-7974"
+    }
+    return result;
 }
