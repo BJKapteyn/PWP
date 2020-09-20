7e925254d709d76be04b8f5af6e79a08f022d18d
Brad <bjkapteyn@gmail.com>
2020-09-20 16:13:01 -0400

    created modal components

--- javascript/modal.js
+++ javascript/modal.js
@@ -1,17 +1,45 @@
-let vectorZones = {
-    zoneB: document.querySelector(""),
-    zoneC: document.querySelector(""),
-    zoneC: document.querySelector(""),
-    zoneC: document.querySelector(""),
-    zoneC: document.querySelector("")
+let modalElements = {
+    modalStandin: document.querySelector("#modalStandin"),
+    modalBackgroudn: document.querySelector("#modalBackground")
 }
 
-window.onload = function {
-    vectorZones = {
-        zoneB: document.querySelector("#zoneB"),
-        zoneC: document.querySelector("#zoneC"),
-        zoneC: document.querySelector("#zoneD"),
-        zoneC: document.querySelector("#zoneF"),
-        zoneC: document.querySelector("#zoneH")
-    }
+function clearElement(parentElement = document.querySelector("")) {
+    parentElement.removeChild(parentElement.firstChild)
+}
+
+//Each zone on the SVG map
+let vectorZones = {
+    zoneB: document.querySelector("#zoneB"),
+    zoneC: document.querySelector("#zoneC"),
+    zoneC: document.querySelector("#zoneD"),
+    zoneC: document.querySelector("#zoneF"),
+    zoneC: document.querySelector("#zoneH")
+}
+
+//contains info on what should go in the modal
+function contactModal(titleStr, zoneStatesStr, zonePresStr, phoneNumberStr) {
+    this.title = titleStr;
+    this.zoneStates = zoneStatesStr;
+    this.zonePres = zonePresStr;
+    this.contactPhone = `tel:+${phoneNumberStr}`;
+}
+
+function modalElements() {
+    this.title = document.createElement("h2");
+    this.zoneStatesTitle = document.createElement("h3");
+    this.zoneStates = document.createElement("p");
+    this.phoneTitle = document.createElement("h3");
+    this.phoneNumber = document.createElement("p");
+    this.emailTitle = document.createElement("h3");
+    this.email = document.createElement("p");
+    this.elements = [this.title, this.zoneStatesTitle, this.zoneStates, this.phoneTitle, this.phoneNumber, this.emailTitle, this.email];
+    
+}
+
+function createModal(contactModal = new contactModal()) {
+
+} 
+
+window.onload = function() {
+
 }
