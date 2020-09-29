8d99329c1b8f20df41fe6491bdb1045f55a0bc9e
Brad <bjkapteyn@gmail.com>
2020-09-28 21:39:51 -0400

    created modal for showing zone info

--- javascript/modal.js
+++ javascript/modal.js
@@ -1,8 +1,8 @@
 let iframe = document.getElementById("iframeMap")
 
 let modalElements = {
-    modalStandin: document.querySelector("#modalStandin"),
-    modalBackgroudn: document.querySelector("#modalBackground")
+    modalStandin: document.getElementById("modalStandin"),
+    modalBackground: document.querySelector("#modalBackground")
 }
 
 function clearElement(parentElement = document.querySelector("")) {
@@ -11,7 +11,12 @@ function clearElement(parentElement = document.querySelector("")) {
 
 //Each zone on the SVG map
 let vectorZones = {
-    
+    zoneB: document.getElementById("zoneB"),
+    zoneC: document.getElementById("zoneC"),
+    zoneD: document.getElementById("zoneD"),
+    zoneF: document.getElementById("zoneF"),
+    zoneH: document.getElementById("zoneH"),
+    zoneL: document.getElementById("zoneL")
 }
 
 //contains info on what should go in the modal
@@ -19,11 +24,62 @@ let vectorZones = {
 function contactModal(zoneStr, zoneStatesStr, zonePresStr, phoneNumberStr = "555-555-5555", zoneChaptersStr, contactEmailStr) {
     this.zone = zoneStr;
     this.zoneStates = zoneStatesStr;
-    this.zoneChapters = zoneChaptersStr;
     this.zonePres = zonePresStr;
+    this.contactPhone = phoneNumberBuilder(phoneNumberStr);
+    this.zoneChapters = zoneChaptersStr;
     this.contactEmail = contactEmailStr;
-    this.contactPhone = phoneNumberstr;
     this.contactSrc = `tel:+${phoneNumberStr}`;
+}
+
+let contactModals = {
+    modalZoneB: new contactModal(
+        "Zone B",
+        "New England area, New York, Pennsylvainia, Maryland",
+        "Don Palermo",
+        "800-637-7974",
+        "Westchester #21; Delco #352; Greater Rochester #683; and Carroll County #935",
+        "pwpdonp@yahoo.com"
+    ),
+    modalZoneC: new contactModal (
+        "Zone C",
+        "",
+        "",
+        "",
+        "",
+        ""
+    ),
+    modalZoneD: new contactModal (
+        "Zone D",
+        "",
+        "",
+        "",
+        "",
+        ""
+    ),
+    modalZoneF: new contactModal (
+        "Zone F",
+        "",
+        "",
+        "",
+        "",
+        ""
+    ),
+    modalZoneH: new contactModal (
+        "Zone H",
+        "",
+        "",
+        "",
+        "",
+        ""
+    ),
+    modalZoneL: new contactModal (
+        "Zone L",
+        "",
+        "",
+        "",
+        "",
+        ""
+    )
 }
 
 function createModalElements(contactModal = new contactModal()) {
@@ -60,24 +116,24 @@ function createModalElements(contactModal = new contactModal()) {
     for(let i = 0; i < elements.length; i++) {
         modal.appendChild(elements[i]);
     }
-
     return modal;
 }
 
-function initializeElements() {
-    vectorZones = {
-        zoneB: iframe.contentWindow.document.getElementById("#zoneB"),
-        zoneC: iframe.contentWindow.document.getElementById("#zoneC"),
-        zoneC: iframe.contentwindow.document.getElementById("#zoneD"),
-        zoneC: iframe.contentwindow.document.getElementById("#zoneF"),
-        zoneC: iframe.contentwindow.document.getElementById("#zoneH")
-    }
-}
-
-function createModal(contactModal = new contactModal()) {
-
+function showModal(contactModal = new contactModal()) {
+    modalElements.modalBackground.style.display = "block";
+    modalElements.modalBackground.style.animationName = "fadeIn";
+    modalElements.modalStandin.appendChild(createModalElements(contactModal));
+    modalElements.modalStandin.style.display = "block";
 } 
 
+function hideModal() {
+    modalElements.modalBackground.style.display = "none";
+    removeChildElement(modalElements.modalStandin)
+}
+
+function removeChildElement(parentElement) {
+    parentElement.removeChild(parentElement.firstChild);
+}
 
 function phoneNumberBuilder(pNumberStr = "555-555-5555") {//build phone number, returns default phone number if nothing is given.
     pNumberArr = pNumberStr.split('-');
@@ -86,10 +142,27 @@ function phoneNumberBuilder(pNumberStr = "555-555-5555") {//build phone number, 
         if(pNumberStr == "555-555-5555") {
             throw "default number";
         }
-        result = `(${pNumberArr[0]}) ${pNumberArr[1]}-${pNumberArr[3]}`
+        result = `1 (${pNumberArr[0]}) ${pNumberArr[1]}-${pNumberArr[2]}`
     }
     catch {
         result = "(800) 637-7974"
     }
     return result;
+}
+
+window.onload = function() {
+    modalElements.modalBackground.addEventListener("click", function() {
+        hideModal();
+    });
+    vectorZones = {
+        zoneB: document.getElementById("zoneB"),
+        zoneC: document.getElementById("zoneC"),
+        zoneD: document.getElementById("zoneD"),
+        zoneF: document.getElementById("zoneF"),
+        zoneH: document.getElementById("zoneH")
+    }
+    vectorZones.zoneB.addEventListener("click", function(event) {
+        event.preventDefault();
+        showModal(contactModals.modalZoneB);
+    })
 }
